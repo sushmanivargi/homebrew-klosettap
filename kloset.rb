b59839b21304da1b0a0b8a4d30b8a231143dce45
Sushma Nivargi <sushma.nivargi@coupoa.com>
2019-06-28 19:41:12 +0530

    Added formula

---
+++ kloset.rb
@@ -0,0 +1,11 @@
+class Kloset < Formula
+  desc ""
+  homepage ""
+  url "http://github.com/sushmanivargi/kloset/archive/v1.0.3.tar.gz"
+  sha256 "122ffa601fcb5b039bbecc0fe10ebbe442485d2f3609df5f50931dfc54a34860"
+
+  def install
+    bin.install "kloset"
+  end
+
+end
